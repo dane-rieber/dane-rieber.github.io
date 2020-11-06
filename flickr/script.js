bf75127f09647ccf54f231946070dca63f566ec2
Dane Rieber <dane.rieber@colorado.edu>
2020-11-05 20:26:46 -0700

    Completed Homework 4

---
+++ flickr/script.js
@@ -0,0 +1,47 @@
+var lastUpdate;
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+        if (Date.now() - lastUpdate < 1000) return;
+        lastUpdate = Date.now();
+        makeApiCall(false);
+    }
+};
+
+var page = 1;
+
+function makeApiCall(refresh) {
+    if (refresh) {
+        $('#results').text('');
+        page = 1;
+    } else {
+        page++;
+    }
+
+    const numPictures = $('#numPictures').find(':selected').text();
+    const searchTerm = $('#searchTerm').val();
+
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=d5e6164057da1dfec8bf6c11c6527842&text=${searchTerm}&per_page=${numPictures}&page=${page}&format=json&nojsoncallback=1`;
+
+    $.ajax(url, {
+        dataType: 'json', success: function(data, status, xhr) {
+            console.log(data);
+
+            var results = '';
+            data.photos.photo.forEach(p => results += getCard(p));
+            $('#results').append(results);
+        }
+    });
+}
+
+function getCard(photo) {
+    return `
+    <div class="col-sm-4 mt-5">
+        <div class="card">
+            <img class="card-img-top" src="https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg" alt="${photo.title}">
+            <div class="card-body">
+                <h5 class="card-title">${photo.title}</h5>
+           </div>
+        </div>
+    </div>`;
+}
